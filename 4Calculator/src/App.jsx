2b76120510ef2899f3cc0370f4127ce73142a6f7
sanjeevsingh22 <skrsingh876@gmail.com>
2024-06-22 13:37:43 +0530

    updated

--- 4Calculator/src/App.jsx
+++ 4Calculator/src/App.jsx
@@ -2,13 +2,26 @@
 import styles from "./App.module.css";
 import Display from "./Components/Display";
 import ButtonContainer from "./Components/ButtonContainer";
-
+import { useState } from "react";
 function App() {
+  let [CalVal, setCalVal]=useState("")
+  const onButtonClick =(buttonText)=>{
+    
+      if (buttonText==='C'){
+        setCalVal("");
+         }else if (buttonText==='='){
+         const result=eval(CalVal);
+          setCalVal(result);
+      }else{
+        const newDisplayValue=CalVal + buttonText
+
+           setCalVal(newDisplayValue);      }
+          }
   
-  return (
-  <div className={styles.calculator}>
-    <Display></Display>
-<ButtonContainer></ButtonContainer>
+        return (
+     <div className={styles.calculator}>
+    <Display displayValue={CalVal}></Display>
+<ButtonContainer onButtonClick={onButtonClick}></ButtonContainer>
   </div>
   );
     
