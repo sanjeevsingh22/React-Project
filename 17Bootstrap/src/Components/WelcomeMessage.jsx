d04ac99d83ee31eec07d4c133c45b8cd5187419d
sanjeevsingh22 <skrsingh876@gmail.com>
2024-06-23 23:17:56 +0530

    update

--- 17Bootstrap/src/Components/WelcomeMessage.jsx
+++ 17Bootstrap/src/Components/WelcomeMessage.jsx
@@ -1,6 +1,6 @@
 
 import styles from "./WelcomeMessage.module.css";
 const WelcomeMessage=()=>{
-    return <p className={styles.welcome}>Enjoy your day</p>;
+    return <p className={styles.welcome}>Enjoy Your Day</p>;
 }
 export default WelcomeMessage;
